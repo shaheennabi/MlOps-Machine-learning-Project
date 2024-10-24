2a19605d34f306f725530ab99e4b368c7d7fcfd5
shaheennabi <ishaheennabi333@gmail.com>
2024-10-24 00:40:16 -0700

    logging updated

--- src/logger/logging.py
+++ src/logger/logging.py
@@ -1,19 +1,19 @@
 import logging
 import os
-from datetime import datetime
-from from_root import from_root
 
+from from_root import from_root
+from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
-log_path = os.path.join(from_root(), 'log', LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
 
-os.makedirs(log_path, exist_ok=True)
-
-lOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=lOG_FILE_PATH,
-    format= "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+   # encoding='utf-8' 
 )
