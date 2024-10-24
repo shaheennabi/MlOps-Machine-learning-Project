e9a63082b100d81540bee221af44abbfbccff6ac
shaheennabi <ishaheennabi333@gmail.com>
2024-10-24 02:10:18 -0700

    data transformation added

---
+++ src/components/data_transformation.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from src.logger.logging import logging
+from src.exception.exception import CustomException
+import os
+import sys 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from  sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
+@dataclass
+class DataTransformationConfig:
+    pass
+
+
+class DataTransformation:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+    def initiate_data_trasformation(self):
+        try:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+        except Exception as e: 
+            logging.info()
+            raise CustomException(e, sys)
+
