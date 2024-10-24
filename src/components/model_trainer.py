28ffc85d8ebfcda60260e7f48b131534d1693cd1
shaheennabi <ishaheennabi333@gmail.com>
2024-10-24 02:20:20 -0700

    model trainer added

---
+++ src/components/model_trainer.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+from src.logger.logging import logging
+from src.exception.exception import CustomException
+import os
+import sys 
+from dataclasses import dataclass
+from pathlib import Path
+from src.utils.utils import save_object,evaluate_model
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+
+
+
+
+@dataclass
+class ModelTrainerConfig:
+    pass
+
+
+class ModelTrainer:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+    def initiate_model_trainer(self):
+        try:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+        except Exception as e: 
+            logging.info()
+            raise CustomException(e, sys)
+
