2eb609b1da192d1fff4ea48492ad6afbf532e06b
shaheennabi <ishaheennabi333@gmail.com>
2024-10-25 02:23:01 -0700

    updated

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -49,13 +49,7 @@ class ModelEvaluation:
                 mlflow.log_metric("r2", r2)
                 mlflow.log_metric("mae", mae)
 
-                 # Model registry does not work with file store
                 if tracking_url_type_store != "file":
-
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
                     mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
                 else:
                     mlflow.sklearn.log_model(model, "model")
